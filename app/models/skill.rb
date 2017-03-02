8edf8761618541dc235d26e35fd0f3abbd8e4b7c
Chris <codemedarkone@gmail.com>
2017-03-01 17:46:56 -0700

    Implement concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,14 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :percent_utilized
+
+   after_initialize :set_defaults
+
+ #@hat set_defaults mean shortcut below
+ #if self.main_image == nil
+ # self.main_image = "http://placehold.it/600x400"
+ #end
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+  end
 end
